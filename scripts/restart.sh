7a629e19b6150d211ab86870c205efcd7ba5797d
prog470dev <tshimizudev4@gmail.com>
2023-09-23 22:20:27 +0900

    ミドルウェアの初期confを追加

---
+++ scripts/restart.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cd `dirname $0`
+
+# nginx
+sudo cp ../conf/nginx/sites-enabled/* /etc/nginx/sites-enabled/
+sudo cp ../conf/nginx/nginx.conf /etc/nginx/nginx.conf
+sudo nginx -t
+sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.`date +%Y%m%d%H%M%S`
+sudo systemctl restart nginx
+
+# mysql
+sudo cp ../conf/mysql/my.cnf /etc/mysql/my.cnf
+sudo mv /var/log/mysql/mysql-slow.log /var/log/mysql/mysql-slow.log.`date +%Y%m%d%H%M%S`
+sudo systemctl restart mysql
+
+# application
+sudo systemctl daemon-reload
+sudo systemctl restart isucondition.go.service
