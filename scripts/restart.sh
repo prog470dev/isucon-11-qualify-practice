07dbafa3bacd2bbe937358fedaea7581996bf932
prog470dev <tshimizudev4@gmail.com>
2023-09-24 00:14:11 +0900

    isu_condition にインデックス(jia_isu_uuid,timestamp)を追加

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -6,12 +6,14 @@ cd `dirname $0`
 sudo cp ../conf/nginx/sites-enabled/* /etc/nginx/sites-enabled/
 sudo cp ../conf/nginx/nginx.conf /etc/nginx/nginx.conf
 sudo nginx -t
-sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.`date +%Y%m%d%H%M%S`
+# sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.`date +%Y%m%d%H%M%S`
+sudo rm /var/log/nginx/access.log
 sudo systemctl restart nginx
 
 # mysql
 sudo cp ../conf/mysql/conf.d/*.cnf /etc/mysql/conf.d/
-sudo mv /var/log/mysql/mysql-slow.log /var/log/mysql/mysql-slow.log.`date +%Y%m%d%H%M%S`
+# sudo mv /var/log/mysql/mysql-slow.log /var/log/mysql/mysql-slow.log.`date +%Y%m%d%H%M%S`
+sudo rm /var/log/mysql/mysql-slow.log
 sudo systemctl restart mysql
 
 # application
---
+++ sql/2_inpr-1.sql
@@ -0,0 +1,1 @@
+ALTER TABLE isucondition.isu_condition ADD INDEX jia_isu_uuid_timestap_idx (jia_isu_uuid, timestamp);
